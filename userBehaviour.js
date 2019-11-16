f9d7260e00e7cceda6b4525b07ab359220828bdd
Taha <taha@Tahas-MacBook-Pro.local>
2019-11-16 19:11:41 +0000

    fixed time

--- userBehaviour.js
+++ userBehaviour.js
@@ -2,7 +2,7 @@
  * @author Taha Al-Jody <taha@ta3design.com>
  * https://github.com/TA3/web-user-behaviour
  */
-var userBehaviour = (function(){
+var userBehaviour = (function () {
     var defaults = {
         userInfo: true,
         clicks: true,
@@ -13,7 +13,7 @@ var userBehaviour = (function(){
         timeCount: true,
         clearAfterProcess: true, // todo
         processTime: 15,
-        processData: function(results){
+        processData: function (results) {
             console.log(results);
         },
     };
@@ -35,24 +35,27 @@ var userBehaviour = (function(){
                 results.clicks.clickCount++;
                 var path = [];
                 var node = "";
-                e.path.forEach((el, i)=>{
-                    if ((i !== e.path.length-1) && (i!== e.path.length-2)) {
+                e.path.forEach((el, i) => {
+                    if ((i !== e.path.length - 1) && (i !== e.path.length - 2)) {
                         node = el.localName;
-                        (el.className !== "") ? el.classList.forEach((clE)=>{node+= "." + clE}): 0;
-                        (el.id !== "") ? node += "#" + el.id : 0;
+                        (el.className !== "") ? el.classList.forEach((clE) => {
+                            node += "." + clE
+                        }): 0;
+                        (el.id !== "") ? node += "#" + el.id: 0;
                         path.push(node);
                     }
                 })
                 path = path.reverse().join(">");
-                results.clicks.clickDetails.push([e.clientX, e.clientY,path,getTimeStamp()]);
+                results.clicks.clickDetails.push([e.clientX, e.clientY, path, getTimeStamp()]);
             },
             mouseMovement: (e) => {
-                mem.mousePosition = [e.clientX, e.clientY,getTimeStamp()];
+                mem.mousePosition = [e.clientX, e.clientY, getTimeStamp()];
             }
         }
     };
     var results = {};
-    function resetResults(){
+
+    function resetResults() {
         results = {
             userInfo: {
                 appCodeName: navigator.appCodeName || '',
@@ -66,49 +69,52 @@ var userBehaviour = (function(){
                 currentTime: 0,
             },
             clicks: {
-                clickCount:0,
+                clickCount: 0,
                 clickDetails: []
             },
             mouseMovements: [],
             mouseScroll: [],
             contextChange: [], //todo
             //keyLogger: [], //todo
-    
+
         }
     };
     resetResults();
-    function getTimeStamp(){
+
+    function getTimeStamp() {
         return Date.now();
     };
-    function config(ob){
+
+    function config(ob) {
         user_config = {};
-        Object.keys(defaults).forEach((i)=>{
+        Object.keys(defaults).forEach((i) => {
             i in ob ? user_config[i] = ob[i] : user_config[i] = defaults[i];
         })
     };
-    function start(){
-        
+
+    function start() {
+
         if (Object.keys(user_config).length !== Object.keys(defaults).length) {
             console.log("no config provided. using default..");
             user_config = defaults;
         }
         // TIME SET
-        if(user_config.timeCount !== undefined && user_config.timeCount){
+        if (user_config.timeCount !== undefined && user_config.timeCount) {
             results.time.startTime = getTimeStamp();
         }
         // MOUSE MOVEMENTS
         if (user_config.mouseMovement) {
             mem.eventListeners.mouseMovement = window.addEventListener("mousemove", mem.eventsFunctions.mouseMovement);
             mem.mouseInterval = setInterval(() => {
-                if(mem.mousePosition && mem.mousePosition.length){//if data has been captured
-                    if(!results.mouseMovements.length || ((mem.mousePosition[0] !== results.mouseMovements[results.mouseMovements.length-1][0]) && (mem.mousePosition[1] !== results.mouseMovements[results.mouseMovements.length-1][1]))){
-                        results.mouseMovements.push(mem.mousePosition) 
+                if (mem.mousePosition && mem.mousePosition.length) { //if data has been captured
+                    if (!results.mouseMovements.length || ((mem.mousePosition[0] !== results.mouseMovements[results.mouseMovements.length - 1][0]) && (mem.mousePosition[1] !== results.mouseMovements[results.mouseMovements.length - 1][1]))) {
+                        results.mouseMovements.push(mem.mousePosition)
                     }
                 }
-            }, defaults.mouseMovementInterval*1000);
-        }   
+            }, defaults.mouseMovementInterval * 1000);
+        }
         //CLICKS
-        if(user_config.clicks){
+        if (user_config.clicks) {
             mem.eventListeners.click = window.addEventListener("click", mem.eventsFunctions.click);
         }
         //SCROLL
@@ -116,20 +122,22 @@ var userBehaviour = (function(){
             mem.eventListeners.scroll = window.addEventListener("scroll", mem.eventsFunctions.scroll);
         }
         //PROCESS INTERVAL
-        if(user_config.processTime !== false){
-            mem.processInterval = setInterval(()=>{
+        if (user_config.processTime !== false) {
+            mem.processInterval = setInterval(() => {
                 user_config.processData(result());
-            },user_config.processTime*1000)
+            }, user_config.processTime * 1000)
         }
     };
-    function processResults(){
+
+    function processResults() {
         user_config.processData(result());
         if (user_config.clearAfterProcess) {
             resetResults();
         }
     }
-    function stop(){
-        if(user_config.processTime !== false){
+
+    function stop() {
+        if (user_config.processTime !== false) {
             clearInterval(mem.processInterval);
         }
         clearInterval(mem.mouseInterval);
@@ -137,20 +145,21 @@ var userBehaviour = (function(){
         window.removeEventListener("click", mem.eventsFunctions.click);
         window.removeEventListener("mousemove", mem.eventsFunctions.mouseMovement);
     }
-    function result(){
+
+    function result() {
         if (user_config.userInfo === false && userBehaviour.showResult().userInfo !== undefined) {
             delete userBehaviour.showResult().userInfo;
         }
-        if(user_config.time.timeCount !== undefined && user_config.time.timeCount){
+        if (user_config.timeCount !== undefined && user_config.timeCount) {
             results.time.currentTime = getTimeStamp();
         }
         return results
     };
-    function showConfig(){
+
+    function showConfig() {
         if (Object.keys(user_config).length !== Object.keys(defaults).length) {
             return defaults;
-        }
-        else{
+        } else {
             return user_config;
         }
     };
